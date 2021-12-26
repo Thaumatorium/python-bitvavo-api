d2ffae720979d8f19aa90d38270080174544bb1b
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2021-12-26 19:27:42 +0100

    base commit; pre-commit's flake8 disabled

---
+++ setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+HERE = Path.cwd()
+
+__version__ = "1.0.0"
+
+with (HERE / "requirements" / "base.txt").open(mode="r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with (HERE / "README.md").open(mode="r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Bitvavo BV and NostraDavid",
+    description="This is an upgraded version of the Python API provided by ",
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISC License",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    name="bitvavo-api-upgraded",
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["tests"]),
+    python_requires=">=3.7",
+    url="https://github.com/Thaumatorium/python-bitvavo-api",
+    version=__version__,
+)
