20140ea757df91a8076f26889880857722fd0260
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2021-12-27 12:21:06 +0100

    add git config to bootstrap
    We now rebase on pull, and pull in tags from the remote (as that does NOT happen auto)

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,3 +6,7 @@ sudo apt-get install python3-distutils python3-apt --yes
 echo "prepare the program for... programming"
 pip install -r requirements/dev.txt
 pip install -e .
+
+echo "setup local git settings"
+git config pull.rebase true  # rebase on pulls, in case of conflicts
+git config remote.origin.tagopt --tags  # pull all tags
