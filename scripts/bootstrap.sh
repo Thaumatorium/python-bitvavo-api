7ae0778909a4d077d5c4b07a5ee1c23c96adbe6b
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2021-12-27 13:33:54 +0100

    improve bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,3 +10,9 @@ pip install -e .
 echo "setup local git settings"
 git config pull.rebase true  # rebase on pulls, in case of conflicts
 git config remote.origin.tagopt --tags  # pull all tags
+
+echo "install pre-commit hooks to prevent you from pushing broken code"
+pre-commit install
+
+echo "set global 'git lg' alias"
+git config --global alias.lg "log --color --graph --abbrev-commit --pretty=format:'%Cred%h %C(bold blue)%an%Creset %Cgreen%ad -%C(yellow)%d%Creset %s' --date=format:'%Y-%m-%d %H:%M'"
