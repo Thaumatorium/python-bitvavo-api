458c6f69df787fae216d551ff97f430ed32bbf4c
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2022-01-15 20:55:39 +0100

    add classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup  # type: ignore
 
 HERE = Path.cwd()
 
@@ -12,7 +12,6 @@ with (HERE / "requirements" / "base.txt").open(mode="r") as requirements_file:
 with (HERE / "README.pypi.md").open(mode="r") as readme_file:
     readme = readme_file.read()
 
-# TODO(NostraDavid) add a classifier after testing several versions; maybe change `python_requires`
 setup(
     author="Bitvavo BV (original code) and NostraDavid (rebuild)",
     description="A unit-tested fork of the Bitvavo API",
@@ -25,6 +24,24 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src", exclude=["tests"]),
     python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Pytest",
+        "Framework :: tox",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
+        "Typing :: Typed",
+    ],
     url="https://github.com/Thaumatorium/python-bitvavo-api",
     version=__version__,
 )
